67a252e16a12c8baaeecc51c7267f3a6bdf31b5f
Audrey Machivenyika <167446129+audreynm2@users.noreply.github.com>
2025-10-26 12:08:46 +0200

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,26 +1,4 @@
-import mysql.connector
-
-try:
-    # Connect to MySQL server
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password=""    
-    )
-
-    if connection.is_connected():
-        cursor = connection.cursor()
-        
-        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-        print("Database 'alx_book_store' created successfully!")
-
-
-except mysql.connector.Error as e:
-    print("Error while connecting to MySQL:", e)
-
-finally:
-    try:
-        cursor.close()
-        connection.close()
-    except:
-        pass
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
