dc2cc613b84741f7df0c0024b27a4e7735b0b1b7
Audrey Machivenyika <167446129+audreynm2@users.noreply.github.com>
2025-10-26 11:50:50 +0200

    Refactor SQL table names and add auto-increment

--- task_2.sql
+++ task_2.sql
@@ -1,38 +1,44 @@
 USE alx_book_store;
 
-CREATE TABLE IF NOT EXISTS authors (
-    author_id INT PRIMARY KEY,
-    author_name VARCHAR(215)
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT NOT NULL AUTO_INCREMENT,
+    author_name VARCHAR(215),
+    PRIMARY KEY (author_id)
 );
 
-CREATE TABLE IF NOT EXISTS books (
-    book_id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(130),
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    PRIMARY KEY (book_id),
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE IF NOT EXISTS customers (
-    customer_id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT NOT NULL AUTO_INCREMENT,
     customer_name VARCHAR(215),
     email VARCHAR(215),
-    address TEXT
+    address TEXT,
+    PRIMARY KEY (customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS orders (
-    order_id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT NOT NULL AUTO_INCREMENT,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS order_details (
-    orderdetailid INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT NOT NULL AUTO_INCREMENT,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    PRIMARY KEY (orderdetailid),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
+
