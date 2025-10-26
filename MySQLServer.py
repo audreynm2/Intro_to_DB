5f20d93a5c9449fc1e091c67bc8b1f9a0080da25
Audrey Machivenyika <167446129+audreynm2@users.noreply.github.com>
2025-10-26 12:05:13 +0200

    Fix exception handling for MySQL connection errors

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,21 +1,21 @@
 import mysql.connector
-from mysql.connector import Error
 
 try:
     # Connect to MySQL server
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password=""
+        password=""    
     )
 
     if connection.is_connected():
         cursor = connection.cursor()
-        # ✅ The checker looks for this exact line ↓
+        
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+
+except mysql.connector.Error as e:
     print("Error while connecting to MySQL:", e)
 
 finally:
